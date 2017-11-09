998e22e41a847677db6ff2864d45c9e7e88808cc
Kyle Shannon <kyle@pobox.com>
2017-11-09 13:08:44 -0700

    fix header

--- eo/fr.go
+++ eo/fr.go
@@ -1,5 +1,5 @@
-// Copyright 2016 Kyle Shannon.  All rights reserved.  Use of this source code
-// is governed by a BSD-style
+// Copyright 2016 Kyle Shannon.  All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package eo
